bf8075b78c51da093f45fcfa17e7f7b6ef2f8ce4
HailuBen1~ <b.hailu.pk@gmail.com>
2023-03-10 19:51:43 -0500

    replace old files with lab 7 files

---
+++ demo.js
@@ -0,0 +1,21 @@
+function functionShowHalloween(){
+    document.getElementById("halloween-div").style.display = "block";
+}
+function functionShowChristmas(){
+    document.getElementById("christmas-div").style.display = "block";
+}
+function functionShowNewYears(){
+    document.getElementById("new-years-div").style.display = "block";
+}
+
+
+function functionHideHalloween(){
+    document.getElementById("halloween-div").style.display = "none";
+}
+function functionHideChristmas() {
+    document.getElementById("christmas-div").style.display = "none";
+}
+function functionHideNewYears(){
+    document.getElementById("new-years-div").style.display = "none";
+}
+
